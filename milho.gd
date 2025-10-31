efe687a2c4631f2357e11337a85d8c5037480087
Alexander Alves <alexanderalves00@hotmail.com>
2025-10-31 13:51:27 -0300

    feat(milho): adicionando milho na plantação
    adicionando milho na plantação

--- milho.gd
+++ milho.gd
@@ -1,24 +1,16 @@
 extends Area2D
-var terra = "nao"
-var plantar = "nao"
-var ja_tem_planta = "nao"
-var pode_colher = "nao"
+#var terra = "nao"
+#var plantar = "nao"
+#var ja_tem_planta = "nao"
+#var pode_colher = "nao"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	$"../segura_informacoes/semente_tomate".text = str("Número de sementes: " ,Dados.semente)
-	
-	if terra == "sim" and ja_tem_planta == "nao" :
-		if Dados.semente >=1 and Input.is_action_just_pressed("milho"):
+	if Input.is_action_just_released("milho"):
 			show()
-			ja_tem_planta = "sim"
-			Dados.semente -=1
+			#ja_tem_planta = "sim"
+			#Dados.semente -=1
 			$AnimatedSprite2D.frame = 0
 			await get_tree().create_timer(3,0).timeout
 			$AnimatedSprite2D.frame = 1
@@ -30,5 +22,12 @@ func _process(delta: float) -> void:
 			$AnimatedSprite2D.frame = 4
 			await get_tree().create_timer(12,0).timeout
 			$AnimatedSprite2D.frame = 5
-	if Input.is_action_just_released("colher") and pode_colher == "sim":
-			queue_free()
+	#if Input.is_action_just_released("colher") and pode_colher == "sim":
+			#queue_free()
+
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
--- tomate.gd
+++ tomate.gd
@@ -3,7 +3,7 @@ extends Area2D
 var terra = "nao"
 var plantar = "nao"
 var ja_tem_planta = "nao"
-var pode_colher = "nao"
+
 
 func _ready() -> void:
 	
@@ -34,10 +34,9 @@ func _process(delta: float) -> void:
 			$Tomate1.frame = 4
 			await get_tree().create_timer(7,0).timeout
 			$Tomate1.frame = 5
-			pode_colher = "sim"
 			
-		if Input.is_action_just_released("colher") and pode_colher == "sim":
-			queue_free()
+			
+
 
 	# aqui ver se ta na terra
 func _on_body_entered(body: Node2D) -> void:
